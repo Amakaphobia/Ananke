ab49447e5ecddb5c66847af105bd639a6499903b
amakaphobia <h.daive89@gmail.com>
2026-07-24 21:44:46 +0200

    prompt

--- home/modules/shell/common/starship.nix
+++ home/modules/shell/common/starship.nix
@@ -8,22 +8,59 @@
       add_newline = false;
       command_timeout = 500;
 
-      format = "$directory$git_branch$git_status$nix_shell$character";
+      # format = "$os $username$hostname $directory$git_branch$git_status$nix_shell$character";
+
+      format = ''
+        $os $username$hostname $nix_shell
+        $directory $git_branch$git_status
+        $character 
+      '';
+      fill.symbol = " ";
+
+      # --- Main Modules ---
+      os = {
+        disabled = false;
+        format = "[$symbol]($style)";
+        style = "bold fg:cyan ";
+        symbols = {
+          NixOS = "";
+        };
+      };
+      username = {
+        disabled = false;
+        format = "[$user]($style)";
+        show_always = true;
+        style_root = "fg:bright-red ";
+        style_user = "fg:purple";
+      };
+
+      hostname = {
+        disabled = false;
+        format = "[@$ssh_symbol$hostname:]($style)";
+        style = "fg:purple";
+        ssh_only = false;
+        ssh_symbol = "󰒋 ";
+      };
 
       directory = {
-        truncation_length = 3;
+        disabled = false;
+        format = "[$path]($style)[$read_only]($read_only_style)";
+        truncation_length = 9;
         truncate_to_repo = true;
+        style = "fg:green";
       };
 
       git_branch = {
-        symbol = "git ";
-        format = "[$symbol$branch]($style) ";
-        style = "bold purple";
+        disabled = false;
+        symbol = " ";
+        format = "[$symbol$branch]($style)";
+        style = "fg:yellow";
       };
 
       git_status = {
-        format = "([$all_status$ahead_behind]($style) )";
-        style = "bold red";
+        disabled = false;
+        format = " ([$all_status$ahead_behind]($style))";
+        style = "fg:yellow";
 
         conflicted = "=";
         ahead = "⇡";
@@ -38,14 +75,17 @@
       };
 
       nix_shell = {
+        disabled = false;
         symbol = "nix ";
-        format = "[$symbol$state( \\($name\\))]($style) ";
+        format = "[$symbol$state(\\($name\\))]($style)";
         style = "bold blue";
       };
 
       character = {
-        success_symbol = "[❯](bold green)";
-        error_symbol = "[❯](bold red)";
+        disabled = false;
+        format = "$symbol";
+        success_symbol = "[](bold green)";
+        error_symbol = "[](bold red)";
       };
     };
   };
