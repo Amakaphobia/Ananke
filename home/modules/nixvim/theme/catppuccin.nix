c05ca2e5d78207c88d9eb47f56dcf6ab8df2dfd3
amakaphobia <h.daive89@gmail.com>
2026-07-25 14:28:24 +0200

    theme

--- home/modules/nixvim/theme/catppuccin.nix
+++ home/modules/nixvim/theme/catppuccin.nix
@@ -1,6 +1,12 @@
 { ... }:
 {
   programs.nixvim = {
-    colorschemes.catppuccin.enable = true;
+    colorschemes.catppuccin = {
+      enable = true;
+      settings = {
+        transparent_background = true;
+        float.transparent = true;
+      };
+    };
   };
 }
