00a39fc335b13dd19a235fd8add03b853483a6fa
amakaphobia <h.daive89@gmail.com>
2026-07-26 09:36:26 +0200

    first steps

--- home/modules/hypr/default.nix
+++ home/modules/hypr/default.nix
@@ -1,7 +1,13 @@
-{ pkgs, ... }:
-
 {
-
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  cfg = config.ananke.desktop.hypr;
+in
+{
   # hypr ecosystem:
   imports = [
     ./land.nix
@@ -10,7 +16,29 @@
     ./lock.nix
   ];
 
-  home.packages = with pkgs; [
-    hyprshutdown
-  ];
+  # create a master switch and a hyprshutdown option
+  options.ananke.desktop.hypr = {
+    enable = lib.mkEnableOption "The ananke Hypr ecosystem";
+
+    shutdown.enable = lib.mkOption {
+      type = lib.types.bool;
+      default = true;
+      description = "wether hyprshutdown should be installed";
+    };
+
+    polkit.enable = lib.mkOption {
+      type = lib.types.bool;
+      default = true;
+      description = "wether hyprpolkit should be installed";
+    };
+
+  };
+
+  # use hyprpolkit by default
+  services.hyprpolkitagent.enable = lib.mkIf (cfg.enable && cfg.polkit.enable);
+
+  #install hyprshutdown alongside hypr by default
+  config = lib.mkIf cfg.enable {
+    home.packages = lib.optional cfg.shutdown.enable pkgs.hyprshutdown;
+  };
 }
