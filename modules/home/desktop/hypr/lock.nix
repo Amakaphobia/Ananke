d442bbb673b5ff4ec4985cee488f85d11f01503e
amakaphobia <29711914+Amakaphobia@users.noreply.github.com>
2026-08-06 13:04:34 +0200

    added clock to hyprlock
    added clock

--- modules/home/desktop/hypr/lock.nix
+++ modules/home/desktop/hypr/lock.nix
@@ -11,6 +11,9 @@ let
   };
   theme = config.ananke.theme;
   colors = theme.scheme.roles;
+  font_family = theme.fonts.sansSerif.name;
+  font_color = colorsLib.hyprRgb colors.foreground;
+  clock_color = colorsLib.hyprRgba colors.accentAlt "99";
 in
 {
   options.ananke.home.desktop.hypr.lock = {
@@ -45,6 +48,69 @@ in
           }
         ];
 
+        shape = [
+          {
+            monitor = "";
+
+            size = "380, 170";
+            position = "0, 300";
+            halign = "center";
+            valign = "center";
+
+            # Slightly tinted glass-like background.
+            color = colorsLib.hyprRgba colors.background "55";
+
+            # Semi-transparent accent border.
+            border_size = 2;
+            border_color = colorsLib.hyprRgba colors.accent "99";
+            rounding = 24;
+
+            shadow_passes = 3;
+            shadow_size = 8;
+            shadow_color = colorsLib.hyprRgba colors.background "99";
+
+            zindex = 0;
+          }
+        ];
+        label = [
+          {
+            text = "cmd[update:1000] date +'%H:%M'";
+
+            halign = "center";
+            valign = "center";
+            text_align = "center";
+            position = "0, 325";
+            monitor = "";
+
+            inherit font_family;
+            color = clock_color;
+            font_size = 64;
+
+            shadow_passes = 2;
+            shadow_size = 4;
+
+            zindex = 1;
+          }
+
+          {
+            text = "cmd[update:60000] date +'%A, %B %-d'";
+
+            halign = "center";
+            valign = "center";
+            text_align = "center";
+            position = "0, 265";
+            monitor = "";
+
+            inherit font_family;
+            color = clock_color;
+            font_size = 20;
+
+            shadow_passes = 1;
+
+            zindex = 1;
+          }
+        ];
+
         input-field = [
           {
             size = "200, 50";
@@ -55,10 +121,9 @@ in
             monitor = "";
 
             dots_center = true;
-            fade_on_empty = false;
+            fade_on_empty = true;
 
-            font_family = theme.fonts.sansSerif.name;
-            font_color = colorsLib.hyprRgb colors.foreground;
+            inherit font_family font_color;
             inner_color = colorsLib.hyprRgb colors.background;
             outer_color = colorsLib.hyprRgb colors.accent;
 
