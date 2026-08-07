66c19911de8f03cdc61dadeab10f8fa31c7cc857
amakaphobia <29711914+Amakaphobia@users.noreply.github.com>
2026-08-07 19:47:27 +0200

    added timer to starship

--- modules/home/shell/common/starship.nix
+++ modules/home/shell/common/starship.nix
@@ -7,6 +7,22 @@
 let
   cfg = config.ananke.home.shell.addons;
   helper = import (paths.lib + "/helper.nix") { inherit lib; };
+
+  timerCommand = ''
+    ms="$ANANKE_CMD_DURATION"
+    if (( ms < 1000 )); then
+        printf '%dms' "$ms"
+
+    elif (( ms < 60000 )); then
+        printf '%.1fs' "$(( ms / 1000.0 ))"
+
+    else
+        minutes=$(( ms / 60000 ))
+        seconds=$(( (ms % 60000) / 1000 ))
+
+        printf '%dm%02ds' "$minutes" "$seconds"
+    fi
+  '';
 in
 {
 
@@ -23,11 +39,14 @@ in
         command_timeout = 500;
 
         format = ''
-          $os $username$hostname $nix_shell
-          $directory $git_branch$git_status
-          $character 
+          [┌](bold fg:purple) $os[ ─ ](bold fg:purple)$username$hostname$nix_shell $fill''${custom.command_success}''${custom.command_failure}[ ┘ ](bold fg:purple)
+          [│](bold fg:purple) $directory $git_branch$git_status
+          [└─](bold fg:purple) $character 
         '';
-        fill.symbol = " ";
+        fill = {
+          symbol = "─";
+          style = "bold fg:purple";
+        };
 
         # --- Main Modules ---
         os = {
@@ -40,7 +59,7 @@ in
         };
         username = {
           disabled = false;
-          format = "[$user]($style)";
+          format = "[ $user]($style)";
           show_always = true;
           style_root = "fg:bright-red ";
           style_user = "fg:purple";
@@ -48,7 +67,7 @@ in
 
         hostname = {
           disabled = false;
-          format = "[@$ssh_symbol$hostname:]($style)";
+          format = "[@$ssh_symbol$hostname: ]($style)";
           style = "fg:purple";
           ssh_only = false;
           ssh_symbol = "󰒋 ";
@@ -88,7 +107,7 @@ in
 
         nix_shell = {
           disabled = false;
-          symbol = "nix ";
+          symbol = "nix";
           format = "[$symbol$state(\\($name\\))]($style)";
           style = "bold blue";
         };
@@ -96,8 +115,27 @@ in
         character = {
           disabled = false;
           format = "$symbol";
-          success_symbol = "[](bold green)";
-          error_symbol = "[](bold red)";
+          success_symbol = "[ -->](purple)";
+          error_symbol = "[ :](bold red)";
+        };
+
+        # display time colored based on exit state
+        custom = {
+          command_success = {
+            when = ''[ -n "$ANANKE_CMD_STATUS" ] && [ "$ANANKE_CMD_STATUS" -eq 0 ]'';
+            command = timerCommand;
+
+            format = "[ 󰅐 $output]($style)";
+            style = "fg:green";
+          };
+
+          command_failure = {
+            when = ''[ -n "$ANANKE_CMD_STATUS" ] && [ "$ANANKE_CMD_STATUS" -ne 0 ]'';
+            command = timerCommand;
+
+            format = "[ 󱡦 $output]($style)";
+            style = "fg:red";
+          };
         };
       };
     };
--- modules/home/shell/zsh/addons.nix
+++ modules/home/shell/zsh/addons.nix
@@ -18,6 +18,23 @@ in
       fzf.enableZshIntegration = lib.mkDefault cfg.fzf.enable;
       # starship
       starship.enableZshIntegration = lib.mkDefault cfg.starship.enable;
+      # make zsh command duration and exit status available to starship
+      zsh.initContent = lib.mkIf cfg.starship.enable (
+        lib.mkAfter ''
+          autoload -Uz add-zsh-hook
+
+          _ananke_starship_export_command_info() {
+            if [[ -v STARSHIP_CMD_STATUS && -v STARSHIP_DURATION ]]; then
+              export ANANKE_CMD_STATUS="$STARSHIP_CMD_STATUS"
+              export ANANKE_CMD_DURATION="$STARSHIP_DURATION"
+            else
+              unset ANANKE_CMD_STATUS ANANKE_CMD_DURATION
+            fi
+          }
+
+          add-zsh-hook precmd _ananke_starship_export_command_info
+        ''
+      );
       # zoxide
       zoxide.enableZshIntegration = lib.mkDefault cfg.zoxide.enable;
     };
