6f863d49d2ae1d890efd03fa67b98e9e38b060d1
amakaphobia <29711914+Amakaphobia@users.noreply.github.com>
2026-08-07 21:50:49 +0200

    added git l and git mffo alias

--- modules/home/cli/git.nix
+++ modules/home/cli/git.nix
@@ -31,14 +31,13 @@ in
           user.Email = "29711914+Amakaphobia@users.noreply.github.com";
 
           alias = {
-            s = "status --short --branch";
             st = "status";
             a = "add --all";
-            c = "commit";
             cm = "commit -m";
             ca = "commit --amend";
-            p = "push";
             pfwl = "push --force-with-lease";
+            l = "log --oneline --decorate --graph --max-count 10";
+            mffo = "merge --ff-only";
           };
           init.defaultBranch = "main";
 
