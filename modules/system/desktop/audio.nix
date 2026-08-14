58c23c6c58384f467adbf8e83d08845528aff6df
amakaphobia <29711914+Amakaphobia@users.noreply.github.com>
2026-08-14 21:48:04 +0200

    config: audio jack

--- modules/system/desktop/audio.nix
+++ modules/system/desktop/audio.nix
@@ -20,8 +20,7 @@ in
         alsa.enable = true;
         alsa.support32Bit = true;
         pulse.enable = true;
-        # If you want to use JACK applications, uncomment this
-        #jack.enable = true;
+        jack.enable = true;
       };
     };
   };
